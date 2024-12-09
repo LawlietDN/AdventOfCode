9fe29730263ef0724d79ffacfaec26a47fb8e64b
LawlietDN <ufcgamer82@gmail.com>
2024-12-09 21:36:43 +0300

    Solved

---
+++ 2024/Day 1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <cmath>    
+
+#include "puzzleInput.h"
+
+int main()
+{
+    std::vector<int> leftList;
+    std::vector<int> rightList;
+
+    for (size_t i = 0; i < puzzleInput.size(); i++)
+    {
+        if (i % 2 == 0)
+        {
+            leftList.push_back(puzzleInput[i]);
+        } else
+        {
+            rightList.push_back(puzzleInput[i]);
+        }
+    }
+
+    std::sort(leftList.begin(), leftList.end());
+    std::sort(rightList.begin(), rightList.end());
+
+    int totalDistance = 0;
+    for (size_t i = 0; i < leftList.size(); i++)
+    {
+        totalDistance += std::abs(leftList[i] - rightList[i]);
+    }
+
+    std::cout << "Total distance: " << totalDistance << std::endl;
+
+    return 0;
+}
